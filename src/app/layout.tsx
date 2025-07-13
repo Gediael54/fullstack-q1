cc5cb98065d081cff3f0a97ef8bd1a49b04584f6
Gediael54 <gediael.trabalho@gmail.com>
2025-07-12 22:59:03 -0300

    Trocando nome do site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Sistema de Login',
+  title: 'Prova técnica',
   description: 'Sistema de autenticação com Next.js',
 };
 
